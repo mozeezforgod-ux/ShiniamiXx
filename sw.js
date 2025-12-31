6cee3dd3545c4ddff036686ea30469ec5b4ebc8d
mozeezforgod-ux <mozeezforgod@gmail.com>
2025-12-30 22:18:33 -1200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+﻿const cacheName = "shinigamix-cache-v1";
+const assetsToCache = [
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/sw.js"
+];
+
+// Install event
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== cacheName)
+        .map(key => caches.delete(key)));
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
